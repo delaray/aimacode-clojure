eea19b6840cfa1ea1ac763134b628e723174d08b
pierre <delaray@hotmail.com>
2018-03-03 13:21:41 +0100

    First commit

---
+++ src/aimacode_clojure/core.clj
@@ -0,0 +1,6 @@
+(ns aimacode-clojure.core)
+
+(defn foo
+  "I don't do a whole lot."
+  [x]
+  (println x "Hello, World!"))
---
+++ test/aimacode_clojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns aimacode-clojure.core-test
+  (:require [clojure.test :refer :all]
+            [aimacode-clojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
